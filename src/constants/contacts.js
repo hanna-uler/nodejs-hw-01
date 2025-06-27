0063808719f0304660f561c26df35ee3ae8c72e0
hanna-uler <hannauler@gmail.com>
2025-06-27 16:16:24 -0400

    working on path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,3 +1,6 @@
 import path from "node:path";
 
-export const PATH_DB = path.join("..", "db", "db.json");
+export const PATH_DB = path.resolve("..", "db", "db.json");
+// console.log(PATH_DB);
+
+// export const PATH_DB = path.join("..", "db", "db.json");
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,8 @@
-const generateContacts = async (number) => {};
+import { readContacts } from "../utils/readContacts.js";
+
+const generateContacts = async (number) => {
+    const data = await readContacts();
+    console.log("data: ", data, ", num: ", number);
+};
 
 generateContacts(5);
