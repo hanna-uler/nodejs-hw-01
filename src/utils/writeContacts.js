14909ff94a7b9c86d63a36558a615f33b69a4205
hanna-uler <hannauler@gmail.com>
2025-06-27 15:27:37 -0400

    fix fs imports

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
