9684af7d90d19b9b10a49e8f926a903c50e88eb5
hanna-uler <hannauler@gmail.com>
2025-06-27 14:53:21 -0400

    fix read func, add write func

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,12 +1,11 @@
-import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
-
+import { PATH_DB } from '../constants/contacts.js';
 export const readContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB, "utf-8");
-        console.log(data);
+        // console.log(JSON.parse(data));
+        return JSON.parse(data);
     } catch (error) {
         console.log(error);
     }
 };
-// console.log(readContacts());
