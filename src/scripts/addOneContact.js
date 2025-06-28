8777d6cf915117230962aa85b45b2f6a6fca8746
hanna-uler <hannauler@gmail.com>
2025-06-28 08:43:50 -0400

    add addOneCont func

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,19 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+await readContacts()
+        .then((data) => {
+            // console.log(data);
+            const newContact = createFakeContact();
+            data.push(newContact);
+            // console.log("updContsData: ", data);
+            return data;
+        })
+        .then(async (data) => {
+            await writeContacts(data);
+        });
+};
 
 addOneContact();
