cfff109a45ea08c43a6b76bc6a4d7026417ba099
hanna-uler <hannauler@gmail.com>
2025-06-28 08:33:15 -0400

    add generateConts func

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,8 +1,27 @@
+import { createFakeContact } from "../utils/createFakeContact.js";
 import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
 
 const generateContacts = async (number) => {
-    const data = await readContacts();
-    console.log("data: ", data, ", num: ", number);
+    await readContacts()
+        .then((data) => {
+            // console.log(data);
+            let currContactsData = data;
+            // console.log(typeof currContactsData);
+            let i = 0;
+            while (i < number) {
+                const newContact = createFakeContact();
+                currContactsData.push(newContact);
+                i++;
+            }
+            // console.log("currContactsData: ", currContactsData);
+            return currContactsData;
+        }
+    )
+        .then(async (data) => {
+            await writeContacts(data);
+        });
 };
 
 generateContacts(5);
