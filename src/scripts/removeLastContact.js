0b578a8391aab06d55cd39fcd91d3c4d590dfbc7
hanna-uler <hannauler@gmail.com>
2025-06-28 09:37:06 -0400

    add removeLast func

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    await readContacts()
+        .then((data) => {
+            let currContactsData = data;
+            return currContactsData;
+        }
+        ).then(async (data) => {
+            if (data.length > 0) {
+                data.pop();
+                await writeContacts(data);
+            } else {
+                return;
+            }
+        });
+};
 
 removeLastContact();
