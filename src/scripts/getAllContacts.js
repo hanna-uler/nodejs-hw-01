e4911f297ffcdb3db4c8674f19e2097443651a92
hanna-uler <hannauler@gmail.com>
2025-06-28 09:08:45 -0400

    add getAllConts func

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,14 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const getAllContacts = async () => {
+    const allContacts = await readContacts().then((data) => {
+        // console.log(data);
+        // console.log(data.length());
+        // console.log(typeof data);
+        return data;
+    });
+    return allContacts;
+
+};
 
 console.log(await getAllContacts());
