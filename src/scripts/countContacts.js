fc9c14338937e97f60f12809e478906331b0a289
hanna-uler <hannauler@gmail.com>
2025-06-28 09:13:28 -0400

    add count func

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,9 @@
-export const countContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+    const allContacts = await readContacts().then((data) => data);
+    // console.log(allContacts.length);
+    return allContacts.length;
+};
 
 console.log(await countContacts());
